ef28c45ee36ef39e314080ffa8664dfed82738a6
Calvin Truong <calvin.truong@aveva.com>
2023-07-28 15:44:56 -0700

    Psuedocode for Integration tests

--- ConsoleAppTests/UnitTest1.cs
+++ ConsoleAppTests/UnitTest1.cs
@@ -6,9 +6,38 @@ namespace ConsoleAppTests
     [TestClass]
     public class UnitTest1
     {
+        //
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //new thread {
+            //  HttpListener listener = new HttpListener(8000)
+            //  thread.Sleep(1000)
+            //}
+        }
+
+
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+         
+
+            //client = new HttpClient(8000)
+
+            //Act
+            //res = client.sendGet()
+
+            //Assert
+            //AssertIsEqual(res.ContentType, "text/html")
+            //AssertIsEqual(res.ContentEncoding, Encoding.UTF8)
+            //AssertGreaterThan(res.ContentLength, 0)
+        }
+
+        [TestCleanup] public void Cleanup()
+        {
+            //listener.shutdown()
         }
     }
 }
