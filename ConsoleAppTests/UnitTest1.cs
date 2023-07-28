52b23d27670b85d1e7b8ad78d09af023e76575d4
Calvin Truong <calvin.truong@aveva.com>
2023-07-28 15:57:54 -0700

    Implemented first TDD failure

--- ConsoleAppTests/UnitTest1.cs
+++ ConsoleAppTests/UnitTest1.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Net;
+using System.Text;
+using System.Web;
 
 namespace ConsoleAppTests
 {
     [TestClass]
     public class UnitTest1
     {
-        //
+        //public Listener {get; set;}
 
         [TestInitialize]
         public void Initialize()
         {
             //new thread {
-            //  HttpListener listener = new HttpListener(8000)
+            //  Listener = new HttpListener(8000)
             //  thread.Sleep(1000)
             //}
         }
@@ -22,22 +25,31 @@ namespace ConsoleAppTests
         public void TestMethod1()
         {
             //Arrange
-         
-
             //client = new HttpClient(8000)
+            HttpListenerResponse res = null;
 
             //Act
             //res = client.sendGet()
 
-            //Assert
-            //AssertIsEqual(res.ContentType, "text/html")
-            //AssertIsEqual(res.ContentEncoding, Encoding.UTF8)
-            //AssertGreaterThan(res.ContentLength, 0)
-        }
 
-        [TestCleanup] public void Cleanup()
+
+            //Assert
+            Assert.AreEqual(
+                expected: res?.ContentType, 
+                "text/html"
+            );
+            Assert.AreEqual(
+                expected:res?.ContentEncoding,
+                Encoding.UTF8
+                );
+            Assert.IsTrue(res?.ContentLength64 > 0);
+        }
+       
+
+        [TestCleanup] 
+        public void Cleanup()
         {
-            //listener.shutdown()
+            //Listener.shutdown()
         }
     }
 }
